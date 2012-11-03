ffc020eb8a33929882f247d47bba718ad1418f49
schnej7 <schnej7@rpi.edu>
2012-11-03 17:01:45 -0400

    added showOnce function to tooltip.js and removed localstore

--- javascripts/localstore.js
+++
@@ -1,9 +0,0 @@
-var localstore = {
-    //Is localStoreage enabled on this browser
-    isEnabled: function(){
-        return (localStorage) ? true : false;
-    },
-
-    //Returns the localStorage object
-    store: localStorage
-};
--- javascripts/tooltip.js
+++ javascripts/tooltip.js
@@ -9,5 +9,16 @@ var tooltip = {
         $(document).click(function(e) {
             a_element.popover('hide')
         });
+    },
+
+    showOnce: function( a_element, a_title, a_content, a_placement ){
+        //Only do this if localStorage is enabled
+        if( localStorage ){
+            var tooltipID = a_element[0].id + a_title + a_content + a_placement;
+            if( !localStorage[tooltipID] ){
+                tooltip.show($('#nav-tutoring'), 'Get Help in Class', "You can get help in any of your CS courses by signing up to be tutored by a member of UPE!");
+                localStorage[tooltipID] = true;
+            }
+        }
     }
 };
